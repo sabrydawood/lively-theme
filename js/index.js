d4ad1caef60515041ce725e0a8cd79bd7f41d5b7
Virus 24 <kazouya44@gmail.com>
2023-09-15 01:32:40 +0300

    test

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ function init(elemid) {
     c = canvas.getContext("2d"),
     w = (canvas.width = window.innerWidth),
     h = (canvas.height = window.innerHeight);
-  c.fillStyle = "rgba(30,30,30,1)";
+  c.fillStyle = "rgba(255,0,0,1)";
   c.fillRect(0, 0, w, h);
   return { c: c, canvas: canvas };
 }
